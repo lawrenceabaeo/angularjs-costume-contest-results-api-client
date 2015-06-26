c32ea72adbf0aae1b3b704ecc9d0b2e039e6825d
Lawrence Abaeo <labaeo@gmail.com>
2015-06-26 13:26:43 -0700

    Add loading text. Update some formatting.

--- js/app.js
+++ js/app.js
@@ -48,3 +48,32 @@ myApp.controller('ContestsController',
           return new Date(dt);
         };
 }]);
+
+// DIRECTIVES
+// http://plnkr.co/edit/bLHm2Y?p=preview
+myApp.directive('loadingText', ['$interval',
+  function ($interval) {
+    return function(scope, element, attrs) {
+      scope.wordArr = [
+        "Loading...", 
+        "How *you* doin'? ;) ", 
+        "Heroku (our data host), is spinning up a new dyno just for you!", 
+        "Do you feel like chicken tonight? Chicken tonight?",
+        "Keep on keepin' on...",
+        ];
+
+      function updateLoadingText(i) {
+        var j=(i+1)%5; //(i+1) to start at the second word, the original author uses modulo so here it is...
+        element.text(scope.wordArr[j]);
+      }
+
+      element.text(scope.wordArr[0]); // Default start
+      stopWord = $interval(updateLoadingText, 3500);
+
+      // listen to DOM destroy (removal) event
+      // to prevent updating word after the DOM element was removed.
+      element.on('destroy', function() {
+        $interval.cancel(stopWord);
+      });
+    }
+}]);
